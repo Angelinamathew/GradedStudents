fe3cd2b659c4bc1d15c85ba541fd1140a0dec60f
Angelinamathew <angelinamathew707@gmail.com>
2024-03-14 23:15:52 -0400

    working on student class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    Double[] examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public Student() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double[] getExamScores() {
+        return examScores;
+    }
+
+    public void setExamScores(Double[] examScores) {
+        this.examScores = examScores;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,23 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Test;
 
+public class StudentTest {
+    Student student = new Student();
+    @Test
+    public void getExamScores() {
+        /// : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        Double[] output = student.getExamScores();
+
+        // Then
+        System.out.println(output);
+
+
+    }
 }

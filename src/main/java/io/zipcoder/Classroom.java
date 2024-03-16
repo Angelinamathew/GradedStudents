1c6a04ba69d38e218c8d4bcbc6f5d0e72f161262
Angelinamathew <angelinamathew707@gmail.com>
2024-03-16 18:48:56 -0400

    Working Classroom test

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
 public class Classroom {
+    Student[] student;
+    int maxNumberOfStudents;
+    public Classroom() {
+        student = new Student[30];
+    }
+
+    public Classroom(int maxNumberOfStudents ){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.student = new Student[maxNumberOfStudents];
+    }
+    public Classroom(int maxNumberOfStudents, Student[] students){
+        this.maxNumberOfStudents =maxNumberOfStudents;
+        this.student = students;
+    }
+
+
+    public Student[] getStudents() {
+        return student;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void testGetStudent(){
+        //Given
+        int maxNoStudents = 10;
+        Student[] students = new Student[]{new Student("Amelia", "mathew",new Double[]{12.0})};
+        Classroom classroom = new Classroom(maxNoStudents,students);
+        //When
+        Student[] expected = students;
+        Student[] actual = classroom.getStudents();
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+    }
 }
